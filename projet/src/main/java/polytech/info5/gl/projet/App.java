8f280aa2665f8ee32378a4df1938bea3c403b8ee
Yann Letourneur <yann.letourneur@etu.univ-grenoble-alpes.fr>
2025-11-03 08:34:57 +0100

    Initial commit

---
+++ projet/src/main/java/polytech/info5/gl/projet/App.java
@@ -0,0 +1,13 @@
+package polytech.info5.gl.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
